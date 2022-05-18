6938e2b2e26eb7b155ff6d3a291cc0b5be44f2b3
Mummuro <kokariev.vld@gmail.com>
2022-05-18 13:33:52 -0400

    Rework of TCP client for py3

--- tcp_client.py
+++ tcp_client.py
@@ -1,11 +1,18 @@
 import socket
-
 target_host = "127.0.0.1"
-target_port = 1234
+target_port = 9998
 
-
+#create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#connect the client
 client.connect((target_host, target_port))
 
-client.send(b"bob mitch")
+#send some data
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+#receive some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
