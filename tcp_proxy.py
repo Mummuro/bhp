72feca33afd4a4b87187015be3f9c99100e3a944
Mummuro <kokariev.vld@gmail.com>
2022-05-20 11:27:39 -0400

    Fixed version

--- tcp_proxy.py
+++ tcp_proxy.py
@@ -4,22 +4,22 @@ import threading
 
 HEX_FILTER = ''.join([(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
 
-def hexdump(src, lenght = 16, show = True):
+def hexdump(src, length = 16, show = True):
         if isinstance(src, bytes):
             src = src.decode()
 
         results = list()
-        for i in range(0, len(src), lenght):
-            word = str(src[i:i+lenght])
+        for i in range(0, len(src), length):
+            word = str(src[i:i+length])
 
             printable = word.translate(HEX_FILTER)
             hexa = ''.join([f'{ord(c):02X}' for c in word])
-            hexwidth = lenght*3
+            hexwidth = length*3
             results.append(f'{i:04x}  {hexa:<{hexwidth}} {printable}')
         if show:
             for line in results:
                 print(line)
-            else:
+        else:
                 return results
 
 def receive_from(connection):
