5bdd054c96feca6eb3c506676f4e56a02404c9ab
Mummuro <kokariev.vld@gmail.com>
2022-06-03 12:22:47 -0400

    get_header function added

--- recapper.py
+++ recapper.py
@@ -11,7 +11,17 @@ PCAPS = '/root/Downloads'
 Response = collections.namedtuple('Response', ['header', 'payload'])
 
 def get_header(payload):
-    pass
+    try:
+        header_raw = payload[:payload.index(b'\r\n\r\n')+2]
+    except ValueError:
+        sys.stdout.write('-')
+        sys.stdout.flush()
+
+        return None
+    header = dict(re.findall(r'(?P<name>.*?): (?P<value>.*?)\r\n', header_raw.decode()))
+    if 'Content-Type' not in header:
+        return None
+    return header
 
 def extract_content(Response, content_name='image'):
     pass
